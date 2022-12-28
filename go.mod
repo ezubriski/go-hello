8d99071f72e7b5323e28574459635251e26455dc
Eric Zubriski <ezubriski@ezubriski.net>
2022-12-28 14:19:28 -0800

    Trying to setup response. Need webhook configuration.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/ezubriski/go-hello
 
 go 1.18
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twilio/twilio-go v1.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	//"strings"
+
+	"github.com/twilio/twilio-go"
+	api "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+/*
+Desired features:
+* Listen to incoming response
+* Process responses as commands
+Command example:
+send PHONE_NUMBER text
+send --times number PHONE NUMBER
+
+*/
 
 func main() {
-	fmt.Println("vim-go")
+	//body := strings.Repeat("♨", 255) fmt.Println(body)
+	//client := twilio.NewRestClient()
+
+	//params := &api.CreateMessageParams{}
+	//params.SetFrom("+13853965242")
+	//params.SetBody(body)
+	//params.SetTo("+15415371994")
+
+	//resp, err := client.Api.CreateMessage(params)
+	//if err != nil {
+	//fmt.Println(err.Error())
+	//} else {
+	//if resp.Sid != nil {
+	//fmt.Println(*resp.Sid)
+	//} else {
+	//fmt.Println(resp.Sid)
+	//}
+	//}
+
+	from := "13853965242"
+	to := "15032676296"
+
+	client := twilio.NewRestClient()
+
+	params := &api.ListMessageParams{}
+	params.SetFrom(from)
+	params.SetTo(to)
+	params.SetPageSize(20)
+	params.SetLimit(100)
+
+	resp, err := client.Api.ListMessage(params)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for record := range resp {
+		fmt.Println("Body: ", *resp[record].Body)
+		fmt.Println("To: ", *resp[record].To)
+		fmt.Println("From: ", *resp[record].From)
+		fmt.Println("")
+	}
+
+	//channel, _ := client.Api.StreamMessage(params)
+	//for record := range channel {
+	//fmt.Println("Body: ", *record.Body)
+	//}
 }
